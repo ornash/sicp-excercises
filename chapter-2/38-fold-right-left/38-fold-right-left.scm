908608bbd63cce580a2ac2a1c3be6ae5aa72907f
ornash <41027459+ornash@users.noreply.github.com>
2020-02-03 19:27:56 -0500

    Submitting 2.38 and 2.39.

--- chapter-2/38-fold-right-left/38-fold-right-left.scm
+++ chapter-2/38-fold-right-left/38-fold-right-left.scm
@@ -2,3 +2,34 @@
 
 ;;;;Requires utilities.scm
 (load "../../utilities/utilities.scm")
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+	result
+	(iter (op result (car rest))
+	      (cdr rest))))
+  (iter initial sequence))
+
+
+(fold-right / 1 (list 1 2 3))
+
+(fold-left / 1 (list 1 2 3))
+
+(fold-right list '() (list 1 2 3))
+
+(fold-left list '() (list 1 2 3))
+;;commutative
+
+;;2.39
+(define (reverse-fold-right sequence)
+  (fold-right (lambda (x y) (append y (list x))) '() sequence))
+
+;;testing
+(reverse-fold-right (list 1 2 3))
+
+(define (reverse-fold-left sequence)
+  (fold-left (lambda (x y) (cons y x)) '() sequence))
+
+;;testing
+(reverse-fold-left (list 1 2 3))
