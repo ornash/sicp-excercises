20ea1f13a144f66d5e87517283670c46b4b9553d
ornash <41027459+ornash@users.noreply.github.com>
2020-11-06 10:14:36 -0500

    Done with chapter 3.

--- chapter-3/73-streams-as-signals/73-streams-as-signals.scm
+++ chapter-3/73-streams-as-signals/73-streams-as-signals.scm
@@ -20,5 +20,36 @@
 (define zero-crossings
   (stream-map sign-change-detector sense-data (cons-stream 0 sense-data)))
 
-;; Skipping 3.75 to 3.82
+;; Skipping 3.75 to 3.76
 ;; Check http://community.schemewiki.org/?sicp-ex-3.75
+
+;; Exercise 3.77
+(define (integral delayed-integrand initial-value dt)
+  (cons-stream initial-value
+	       (let ((integrand (force delayed-integrand)))
+		 (if (stream-null? integrand)
+		     the-empty-stream
+		     (integral (delay (stream-cdr integrand))
+			       (+ (* dt (stream-car integrand))
+				  initial-value)
+			       dt)))))
+
+;; Exercise 3.78
+(define (solve-2nd a b dt y0 dy0)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (integral (delay ddy) dy0 dt))
+  (define ddy (add-streams (scale-stream dy a) (scale-stream y b)))
+  y)
+
+;; Exercise 3.79
+(define(general-solve-2nd f y0 dy0 dt)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (integral (delay ddy) dy0 dt))
+  (define ddy (stream-map f dy y))
+  y)
+
+
+;; Skipping 3.80 to 3.82
+;; Check http://community.schemewiki.org/?sicp-ex-3.80
+
+
